0f9eeb94b4fe99d82ce6a763cd3873dccb084592
Evan Allen <evanallen13@github.com>
2025-08-30 17:17:08 +0000

    add wf

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
+import sys
 from openai import OpenAI
 
 token = os.environ["GITHUB_TOKEN"]
 endpoint = "https://models.github.ai/inference"
 model = "openai/gpt-5-mini"
-prompt = os.environ["PROMPT"]
+
+user_input = sys.argv[1] if len(sys.argv) > 1 else None
+if not user_input:
+    print("No prompt provided.")
+    sys.exit(1)
+    
+source_lang="English"
+target_lang="French"
+
+prompt=f"Translate the following {source_lang} text to {target_lang}: {user_input}"
 
 client = OpenAI(
     base_url=endpoint,
@@ -15,7 +25,7 @@ response = client.chat.completions.create(
     messages=[
         {
             "role": "system",
-            "content": "You are a helpful assistant.",
+            "content": "You are a helpful iterator that translates text from one language to another.",
         },
         {
             "role": "user",
