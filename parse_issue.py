a8d01093b234e04dddac651984f5b32642101b50
Evan Allen <evanallen13@github.com>
2025-09-21 17:57:30 +0000

    test

--- parse_issue.py
+++ parse_issue.py
@@ -11,9 +11,9 @@ github_env = os.environ.get('GITHUB_ENV')
 if github_env:
     with open(github_env, 'a') as env_file:
         print("Ran in GitHub Actions environment, writing to GITHUB_ENV")
-        if source_lang:
-            print(f'SOURCE_LANG={source_lang.group(1).strip()}')
-            env_file.write(f'SOURCE_LANG={source_lang.group(1).strip()}\n')
+        # if source_lang:
+        #     print(f'SOURCE_LANG={source_lang.group(1).strip()}')
+        env_file.write(f'SOURCE_LANG={source_lang.group(1).strip()}\n')
         if target_lang:
             print(f'TARGET_LANG={target_lang.group(1).strip()}')
             env_file.write(f'TARGET_LANG={target_lang.group(1).strip()}\n')
