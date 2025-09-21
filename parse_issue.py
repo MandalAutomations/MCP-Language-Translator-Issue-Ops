2f48e322e126536a0285ccb395a655151b5e49ba
Evan Allen <evanallen13@github.com>
2025-09-21 17:30:33 +0000

    add debugging

--- parse_issue.py
+++ parse_issue.py
@@ -9,6 +9,8 @@ target_lang = re.search(r'target_lang:\s*(.+)', body)
 
 github_env = os.environ.get('GITHUB_ENV')
 if github_env:
+    print("Ran in GitHub Actions environment, writing to GITHUB_ENV")
+if github_env:
 	with open(github_env, 'a') as env_file:
 		if source_lang:
 			env_file.write(f'SOURCE_LANG={source_lang.group(1).strip()}\n')
